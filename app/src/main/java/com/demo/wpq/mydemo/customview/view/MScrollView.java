472bc57d991a00744c0e3ebc0cdf3cb97dc6b243
wpq2014 <wpq0358@163.com>
2016-07-12 11:34:28 +0800

    解决ScrollView嵌套ViewPager上下左右滚动冲突； 触摸ScrollView弹回输入法键盘，针对ScrollView里有EditText的情况

---
+++ app/src/main/java/com/demo/wpq/mydemo/customview/view/MScrollView.java
@@ -0,0 +1,63 @@
+package com.demo.wpq.mydemo.customview.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.widget.ScrollView;
+
+import com.demo.wpq.mydemo.utils.KeyboardUtil;
+
+/**
+ * Desc: 解决ScrollView嵌套ViewPager上下左右滚动冲突；
+ *       触摸ScrollView弹回输入法键盘，针对ScrollView里有EditText的情况
+ *
+ * Created by wpq on 16/7/12.
+ */
+public class MScrollView extends ScrollView {
+
+    private float xDistance, yDistance, xLast, yLast;
+
+    public MScrollView(Context context) {
+        super(context);
+    }
+
+    public MScrollView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MScrollView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+//    public MScrollView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+//        super(context, attrs, defStyleAttr, defStyleRes);
+//    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        switch (ev.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                xDistance = yDistance = 0f;
+                xLast = ev.getX();
+                yLast = ev.getY();
+                break;
+            case MotionEvent.ACTION_MOVE:
+                float xCurr = ev.getX();
+                float yCurr = ev.getY();
+
+                xDistance += Math.abs(xCurr - xLast);
+                yDistance += Math.abs(yCurr - yLast);
+                if (xDistance > yDistance) {
+                    return false;
+                }
+                break;
+        }
+        return super.onInterceptTouchEvent(ev);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        KeyboardUtil.hideKeyboard(this);
+        return super.onTouchEvent(ev);
+    }
+}
---
+++ app/src/main/java/com/demo/wpq/mydemo/utils/KeyboardUtil.java
@@ -0,0 +1,86 @@
+package com.demo.wpq.mydemo.utils;
+
+import android.content.Context;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class KeyboardUtil {
+
+	// 隐藏虚拟键盘
+	public static void hideKeyboard(View v) {
+		InputMethodManager imm = (InputMethodManager) v.getContext()
+				.getSystemService(Context.INPUT_METHOD_SERVICE);
+		if (imm.isActive()) {
+			imm.hideSoftInputFromWindow(v.getApplicationWindowToken(), 0);
+
+		}
+	}
+
+	// 切换软键盘的状态 如当前为收起变为弹出,若当前为弹出变为收起
+	public static void hideKeyboard(Context context) {
+		InputMethodManager imm = (InputMethodManager) context
+				.getSystemService(Context.INPUT_METHOD_SERVICE);
+		imm.toggleSoftInput(0, InputMethodManager.HIDE_NOT_ALWAYS);
+	}
+
+	// 显示虚拟键盘
+	public static void showKeyboard(View v) {
+		InputMethodManager imm = (InputMethodManager) v.getContext()
+				.getSystemService(Context.INPUT_METHOD_SERVICE);
+
+		imm.showSoftInput(v, InputMethodManager.SHOW_FORCED);
+
+	}
+
+	// 强制显示或者关闭系统键盘
+	public static void KeyBoard(final EditText txtSearchKey, final String status) {
+
+		Timer timer = new Timer();
+		timer.schedule(new TimerTask() {
+			@Override
+			public void run() {
+				InputMethodManager m = (InputMethodManager) txtSearchKey
+						.getContext().getSystemService(
+								Context.INPUT_METHOD_SERVICE);
+				if (status.equals("open")) {
+					m.showSoftInput(txtSearchKey,
+							InputMethodManager.SHOW_FORCED);
+				} else {
+					m.hideSoftInputFromWindow(txtSearchKey.getWindowToken(), 0);
+				}
+			}
+		}, 300);
+	}
+
+	// 通过定时器强制隐藏虚拟键盘
+	public static void TimerHideKeyboard(final View v) {
+		Timer timer = new Timer();
+		timer.schedule(new TimerTask() {
+			@Override
+			public void run() {
+				InputMethodManager imm = (InputMethodManager) v.getContext()
+						.getSystemService(Context.INPUT_METHOD_SERVICE);
+				if (imm.isActive()) {
+					imm.hideSoftInputFromWindow(v.getApplicationWindowToken(),
+							0);
+				}
+			}
+		}, 10);
+	}
+
+	// 输入法是否显示着
+	public static boolean KeyBoard(EditText edittext) {
+		boolean bool = false;
+		InputMethodManager imm = (InputMethodManager) edittext.getContext()
+				.getSystemService(Context.INPUT_METHOD_SERVICE);
+		if (imm.isActive()) {
+			bool = true;
+		}
+		return bool;
+
+	}
+}
