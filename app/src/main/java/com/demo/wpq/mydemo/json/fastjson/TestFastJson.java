d01457175f223c1d82df6cb569466e2c1672e2b6
WuPuquan <xingren@7lk.com>
2017-07-10 19:25:59 +0800

    测试JSONObject.parseObject(String) null 和 “” 北过滤的情况

--- app/src/main/java/com/demo/wpq/mydemo/json/fastjson/TestFastJson.java
+++ app/src/main/java/com/demo/wpq/mydemo/json/fastjson/TestFastJson.java
@@ -5,12 +5,14 @@ import com.alibaba.fastjson.JSONObject;
 import com.demo.wpq.mydemo.bean.HotBean;
 import com.demo.wpq.mydemo.json.Person;
 
+import static com.alibaba.fastjson.JSON.parseObject;
+
 public class TestFastJson {
 
     public static void main(String[] args) {
         //
         String json = "{\"name\":\"fastjson\",\"age\":10}";
-        Person p1 = JSON.parseObject(json, Person.class);
+        Person p1 = parseObject(json, Person.class);
         System.out.println(p1.toString());
 
         //
@@ -18,7 +20,7 @@ public class TestFastJson {
         o2.put("name", "fastjson2");
         o2.put("age", 20);
         System.out.println(o2.toString());
-        Person p2 = JSON.parseObject(o2.toString(), Person.class);
+        Person p2 = parseObject(o2.toString(), Person.class);
         System.out.println(p2.toString());
 
         //
@@ -29,8 +31,26 @@ public class TestFastJson {
         System.out.println(o3);
 
         String hotJson = "{\"msg\":\"获取成功！\",\"code\":1,\"data\":{\"list\":[{\"name\":\"三只松鼠\"}]}}";
-        HotBean hotBean = JSON.parseObject(hotJson, HotBean.class);
+        HotBean hotBean = parseObject(hotJson, HotBean.class);
         System.out.println(hotBean.toString());
 
+
+        String data = "{\n" +
+                "  \"status\" : 1,\n" +
+                "  \"errorCode\" : \"\",\n" +
+                "  \"message\" : null,\n" +
+                "  \"data\" : {\n" +
+                "    \"preWeekScore\" : 0.0,\n" +
+                "    \"thisWeekScore\" : 0.0,\n" +
+                "    \"taskName\" : \"课堂点名\",\n" +
+                "    \"taskTime\" : \"15秒\",\n" +
+                "    \"taskColor\" : 1,\n" +
+                "    \"isShow\" : 1\n" +
+                "  },\n" +
+                "  \"erroCode\" : null\n" +
+                "}";
+        JSONObject jsonObject = JSONObject.parseObject(data);
+//        String string = JSON.toJSONString(jsonObject, SerializerFeature.WriteMapNullValue);
+        System.out.println(jsonObject);
     }
 }
